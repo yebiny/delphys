d09d84f0297dd954c6be39251fb051f0a3d8229e
slowmoyang <slowmoyang@gmail.com>
2018-09-28 17:47:44 +0900

    Update MathUtils

--- external/interface/MathUtils.h
+++ external/interface/MathUtils.h
@@ -3,35 +3,25 @@
 
 #include "TMath.h"
 
-#include <cmath>
 #include <iostream>
+#include <cmath>
+#include <tuple>
 
 namespace delphys {
 
 static const Double_t kPi = TMath::Pi();
 static const Double_t kTwoPi = TMath::TwoPi();
 
-inline Double_t ComputeDeltaPhi(Double_t phi1, Double_t phi2) {
-  Double_t dphi = phi1 - phi2;
-  if(TMath::IsNaN(dphi)) {
-    std::cerr << "ComputeDeltaPhi function called with NaN" << std::endl;
-    return dphi;
-  }
-  while (dphi >= kPi) dphi -= kTwoPi;
-  while (dphi < -kPi) dphi += kTwoPi;
-  return dphi;
-}
+Double_t ComputeDeltaPhi(Double_t phi1, Double_t phi2);
+Double_t ComputeDeltaR(Double_t deta, Double_t dphi);
 
-inline Double_t ComputeDeltaR(Double_t deta, Double_t dphi) {
-  return std::hypot(deta, dphi);
-}
+Double_t ComputeDeltaR(Double_t eta1, Double_t eta2,
+                       Double_t phi1, Double_t phi2);
 
-inline Double_t ComputeDeltaR(Double_t eta1, Double_t eta2,
-                              Double_t phi1, Double_t phi2) {
-  Double_t deta = eta1 - eta2;
-  Double_t dphi = ComputeDeltaPhi(phi1, phi2);
-  return ComputeDeltaR(deta, dphi);
-}
+std::tuple<Double_t, Double_t> ComputeAxes(
+    const std::vector<Double_t> & x_values,
+    const std::vector<Double_t> & y_values,
+    const std::vector<Double_t> & weights);
 
 } // NOTE end of delphys namespace
 
